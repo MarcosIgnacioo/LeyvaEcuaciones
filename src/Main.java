93e9946e958a9d66a47423f777f9eca6cc9f9fff
Marcos <magma_ruby145@hotmail.com>
2023-04-26 16:13:54 -0700

    123

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -5,59 +7,48 @@ public class Main {
         double m = 0;
         double mAnt = 0;
         double tolerancia = 0.001;
-        double aVar = 1;
-        double bVar = 2;
-
-        AXe Ma = new AXe(1,m,3);
-        BX Mb = new BX(-3,m);
-        C Mc = new C(1);
-
-        AXe Aa = new AXe(1,aVar,3);
-        BX Ab = new BX(-3,aVar);
-        C Ac = new C(1);
-        Ecuacion eA = new Ecuacion(Aa,Ab,Ac);
-
-        AXe Ba = new AXe(1,bVar,3);
-        BX Bb = new BX(-3,bVar);
-        C Bc = new C(1);
-        Ecuacion eB = new Ecuacion(Ba,Bb,Bc);
-        double valorA = eA.getEcuacionResultado();
-        double valorB = eB.getEcuacionResultado();
-        Ecuacion eM;
-        
-        /*
+        double a = 1;
+        double b = 2;
+        double fA = ecuacion(a);
+        double fb = ecuacion(b);
+        System.out.println(ecuacion(1));
+        System.out.println(ecuacion(2));
+        int i = 0;
+        double error = 1;
         do{
-            if ((valorA*valorB) < 0){
-                mAnt = m;
-                m = (aVar + bVar)/2;
-                eM = new Ecuacion(Ma, Mb, Mc);
-                double valorM = eM.getEcuacionResultado();
-                System.out.println(valorM*valorA);
-                if ((valorA*valorM) < 0){
-                    bVar = m;
-                    eB = ejecutar(eB,bVar, Ba, Bb, Bc);
-                    valorB = eB.getEcuacionResultado();
-                    System.out.println(eB.getEcuacionResultado());
+            if ((fA*fb) < 0){
+                m = (a + b)/2;
+                double fM = ecuacion(m);;
+                error = Math.abs((m-mAnt)/m);
+                System.out.println("it: ["+(i++)+"] a: ["+a+"] b: ["+b+"] m: ["+m+"] f(a): ["+fA+"] f(b): ["+fb+"] f(m): ["+fM+"] f(a)*f(m) ["+ (fA*fM)+"]"+" Error:["+error+"]");
+
+                if ((fA*fM) < 0){
+                    b = m;
+                    fb = ecuacion(b);
                 }
                 else{
-                    aVar = m;
-                    eA = ejecutar(eA,aVar, Aa, Ab, Ac);
-                    valorA = eA.getEcuacionResultado();
-                    System.out.println(eA.getEcuacionResultado());
+                    a = m;
+                    fA = ecuacion(a);
                 }
             }
+            mAnt = m;
+        }while(error>tolerancia);
 
-        }
-        while(m>tolerancia);*/
     }
-
     public static Ecuacion ejecutar(Ecuacion eX, double x, AXe aTmp, BX bTmp, C cTmp){
         aTmp.setX(x);
         bTmp.setX(x);
         eX.setAxe(aTmp.getAxe());
         eX.setAxe(bTmp.getBx());
         eX.setAxe(cTmp.getC());
+        eX.getEcuacionResultado();
         Ecuacion eN = new Ecuacion(aTmp,bTmp,cTmp);
         return eN;
     }
+
+    public static double ecuacion(double x){
+        return (Math.pow(x,3))-3*x+1;
+    }
+
+
 }
